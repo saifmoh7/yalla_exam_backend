09447516931a77ea60d29dd5de3d069238f15f69
saifmoh7 <saif.2017.moh@gmail.com>
2023-11-29 21:49:05 +0300

    deploy verify3

--- models/mailerModel.js
+++ models/mailerModel.js
@@ -17,40 +17,20 @@ const sendMailer = async (userEmail, htmlTemp) => {
       html: htmlTemp
     }
 
-    const info = transporter.sendMail(mailOptions)
-    console.log("email send" + info.response)
+    const info = await transporter.sendMail(mailOptions)
+    console.log(info)
+    if (typeof info === 'object' && typeof info.messageId === 'string') {
+      return {status:"ok", data:info}
+    }else{
+      return {status:"bad request", error: info}
+    }
   } catch (error) {
-    
+    return {status:"error", error}
   }
 }
-
+// sendMailer('laithmohammedsaker@gmail.com',"test hello world").then(log=>console.log(log))
 module.exports = sendMailer
 
 
-
-// // async..await is not allowed in global scope, must use a wrapper
-// async function main() {
-//   // send mail with defined transport object
-//   const info = await transporter.sendMail({
-//     from: '"Fred Foo 👻" <foo@example.com>', // sender address
-//     to: "bar@example.com, baz@example.com", // list of receivers
-//     subject: "Hello ✔", // Subject line
-//     text: "Hello world?", // plain text body
-//     html: "<b>Hello world?</b>", // html body
-//   });
-
-//   console.log("Message sent: %s", info.messageId);
-//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-//   //
-//   // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-//   //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-//   //       <https://github.com/forwardemail/preview-email>
-//   //
-// }
-
-// main().catch(console.error);
-
-// module.exports = transporter;
 
 // // 8UrL=8[r=8xf
